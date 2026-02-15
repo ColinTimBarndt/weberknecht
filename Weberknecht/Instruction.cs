1a5a1aa947bc396a4712a0e0aaab9cc0dc72f34c
Colin Tim Barndt <contact@colintimbarndt.de>
2026-02-15 21:43:01 +0100

    Improved instruction to string

--- Weberknecht/Instruction.cs
+++ Weberknecht/Instruction.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection.Emit;
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
 using Weberknecht.Metadata;
 
 namespace Weberknecht;
@@ -27,20 +29,84 @@ public struct Instruction
             return $"{prefix}{OpCode}";
         }
 
-        switch (OpCode.OperandType)
+        if (OpCode.OperandType is OperandType.InlineBrTarget or OperandType.ShortInlineBrTarget)
+            return $"{prefix}{OpCode} L{_operand:X4}";
+
+        StringBuilder builder = new();
+        builder.Append(prefix).Append(OpCode).Append(' ');
+        return (_operand switch
         {
-            case OperandType.InlineBrTarget or OperandType.ShortInlineBrTarget:
-                return $"{prefix}{OpCode} L{_operand:X4}";
-            default:
-                break;
+            string s => FormatLiteral(builder, s),
+            MethodInfo method => FormatMethod(builder, method),
+            ConstructorInfo ctor => FormatConstructor(builder, ctor),
+            _ => builder.Append(_operand),
+        }).ToString();
+    }
+
+    private static StringBuilder FormatLiteral(StringBuilder builder, string literal)
+    {
+        builder.Append('"');
+        foreach (char ch in literal)
+        {
+            switch (ch)
+            {
+                case '"':
+                case '\'':
+                case '\\':
+                    builder.Append('\\').Append(ch);
+                    continue;
+
+                case '\n':
+                    builder.Append("\\n");
+                    continue;
+
+                case '\r':
+                    builder.Append("\\r");
+                    continue;
+
+                case '\f':
+                    builder.Append("\\f");
+                    continue;
+
+                default:
+                    if (ch < 0x20)
+                        builder.AppendFormat("\\x{0:02x}", ch);
+                    else
+                        builder.Append(ch);
+                    continue;
+            }
+        }
+        builder.Append('"');
+        return builder;
+    }
+
+    private static StringBuilder FormatMethod(StringBuilder builder, MethodInfo method)
+    {
+        builder.Append(method.ReturnType).Append(' ');
+
+        if (method.DeclaringType is Type declType)
+            builder.Append(declType.Name).Append("::");
+
+        builder.Append(method.Name);
+
+        var generics = method.GetGenericArguments();
+        if (generics.Length > 0)
+        {
+            builder.Append('<').AppendJoin(", ", generics.Select(t => t.Name)).Append('>');
         }
 
-        if (_operand is string s)
-        {
-            return $"{prefix}{OpCode} \"{s}\"";
-        }
+        builder.Append('(').AppendJoin(", ", method.GetParameters()).Append(')');
 
-        return $"{prefix}{OpCode} {_operand}";
+        return builder;
+    }
+
+    private static StringBuilder FormatConstructor(StringBuilder builder, ConstructorInfo ctor)
+    {
+        builder.Append(ctor.DeclaringType?.Name ?? "<unnamed>");
+
+        builder.Append('(').AppendJoin(", ", ctor.GetParameters()).Append(')');
+
+        return builder;
     }
 
 }
