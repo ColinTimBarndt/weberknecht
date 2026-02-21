99ad3a0d2b3285c1a4021353accf1418388d5b2d
Colin Tim Barndt <contact@colintimbarndt.de>
2026-02-21 08:26:22 +0100

    Split files for instructions

---
+++ Weberknecht/PseudoInstruction.cs
@@ -0,0 +1,98 @@
+using System.Text;
+
+namespace Weberknecht;
+
+public struct PseudoInstruction()
+{
+
+	public readonly PseudoInstructionType Type { get; } = PseudoInstructionType.Instruction;
+
+	// managed type
+	internal Instruction _instruction = new();
+
+	internal int _label;
+
+	public override readonly string ToString() => new StringBuilder().Append(in this).ToString();
+
+	private PseudoInstruction(PseudoInstructionType tag) : this()
+	{
+		Type = tag;
+	}
+
+	private PseudoInstruction(Instruction instr) : this(PseudoInstructionType.Instruction)
+	{
+		_instruction = instr;
+	}
+
+	private PseudoInstruction(int label) : this(PseudoInstructionType.Label)
+	{
+		_label = label;
+	}
+
+	public static implicit operator PseudoInstruction(Instruction instr) => new(instr);
+
+	public static PseudoInstruction Label(int label) => new(label);
+
+	public static PseudoInstruction Try() => new(PseudoInstructionType.Try);
+
+	public static PseudoInstruction Catch() => new(PseudoInstructionType.Catch);
+
+	public static PseudoInstruction Finally() => new(PseudoInstructionType.Finally);
+
+}
+
+public enum PseudoInstructionType : byte
+{
+	Instruction,
+	Label,
+	Try,
+	Catch,
+	Finally,
+}
+
+// Extension class to receive 'this' by ref
+public static class PseudoInstructionExt
+{
+
+	public static ref Instruction AsInstructionRef(ref this PseudoInstruction self)
+	{
+		if (self.Type != PseudoInstructionType.Instruction)
+			throw new InvalidOperationException();
+		return ref self._instruction;
+	}
+
+	public static Instruction AsInstruction(this PseudoInstruction self)
+	{
+		if (self.Type != PseudoInstructionType.Instruction)
+			throw new InvalidOperationException();
+		return self._instruction;
+	}
+
+	public static ref int AsLabelRef(ref this PseudoInstruction self)
+	{
+		if (self.Type != PseudoInstructionType.Label)
+			throw new InvalidOperationException();
+		return ref self._label;
+	}
+
+	public static int AsLabel(this PseudoInstruction self)
+	{
+		if (self.Type != PseudoInstructionType.Label)
+			throw new InvalidOperationException();
+		return self._label;
+	}
+
+	public static StringBuilder Append(this StringBuilder builder, in PseudoInstruction self)
+	{
+		return self.Type switch
+		{
+			PseudoInstructionType.Instruction => builder.Append(in self._instruction),
+			PseudoInstructionType.Label => builder.AppendFormat("L{0:X}:", self._label),
+			PseudoInstructionType.Try => builder.Append("try:"),
+			PseudoInstructionType.Catch => builder.Append("catch:"), //TODO
+			PseudoInstructionType.Finally => builder.Append("finally:"),
+			_ => throw new NotImplementedException(Enum.GetName(self.Type)),
+		};
+	}
+
+}
