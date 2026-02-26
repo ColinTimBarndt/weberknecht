687397f3b10625286f929fb74b37c9665e5e96ef
Colin Tim Barndt <contact@colintimbarndt.de>
2026-02-26 10:47:26 +0100

    Update equality on Instruction for labels

--- Weberknecht/Instruction/IEquatable.cs
+++ Weberknecht/Instruction/IEquatable.cs
@@ -6,18 +6,34 @@ public partial struct Instruction : IEquatable<Instruction>
     public readonly bool Equals(Instruction other)
         => OpCode == other.OpCode
         && _operand == other._operand
-        && _uoperand.@long == other._uoperand.@long;
+        && _uoperand.@long == other._uoperand.@long
+        && _label == other.Label;
 
     public override readonly bool Equals(object? obj)
         => obj is Instruction instr && Equals(instr);
 
     public override readonly int GetHashCode()
         => _operand == null
-            ? HashCode.Combine(OpCode, _uoperand.@long)
-            : HashCode.Combine(OpCode, _operand);
+            ? HashCode.Combine(_label, OpCode, _uoperand.@long)
+            : HashCode.Combine(_label, OpCode, _operand);
 
     public static bool operator ==(Instruction left, Instruction right) => left.Equals(right);
 
     public static bool operator !=(Instruction left, Instruction right) => !left.Equals(right);
+
+    public readonly struct LabelIgnoringComparer() : IEqualityComparer<Instruction>
+    {
+
+        public bool Equals(Instruction x, Instruction y)
+            => x.OpCode == y.OpCode
+            && x._operand == y._operand
+            && x._uoperand.@long == y._uoperand.@long;
+
+        public int GetHashCode(Instruction obj)
+            => obj._operand == null
+                ? HashCode.Combine(obj.OpCode, obj._uoperand.@long)
+                : HashCode.Combine(obj.OpCode, obj._operand);
+
+    }
 
 }
